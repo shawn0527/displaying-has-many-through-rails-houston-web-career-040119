aecf5b0bba2f61e24b2156273db34fbd87ddc119
Shawn Ren <shawnren527@gmail.com>
2019-04-30 20:15:51 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
 
+  before_action :current_user, except: [:index, :new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def current_user
+    User.find(params[:id])
+  end
+
 end
